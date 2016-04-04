27cc9f25aaeed18ecdee34ae7ae434199c7e5c69
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-04-04 16:02:48 +0300

    [Ticket #4] Configure Webpack

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+(function() {
+  'use strict';
+  var gulp = require('gulp'),
+  less = require('gulp-less'),
+  source = require()
+})();
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+
+  module.exports = {
+    entry: './app/scripts/trial.js',
+    output: {
+      filename: './public/js/bundle.js'
+    },
+    watch: true
+  };
+})();
