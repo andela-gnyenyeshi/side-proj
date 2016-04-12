351f5490cab28f7de06467f897c701bf4846c085
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-04-12 11:04:19 +0300

    [Ticket #9] Adds duplicate member message

--- server/controllers/members.js
+++ server/controllers/members.js
@@ -8,16 +8,27 @@
       var member = new Member();
       member.name = req.body.name;
       member.isMember = req.body.member;
-      member.save(function(err, member) {
-        if (err) {
-          return res.status(500).json({
-            'error': 'Error saving new Member',
-            err: err || err.errmessage
+
+      Member.find({
+        name: member.name
+      }, function(err, exist) {
+        if(exist.length > 0) {
+          return res.status(409).json({
+            'message': 'Sorry, this member already exists'
           });
-        } else if (member) {
-          return res.status(200).json({
-            'message': 'Successfully created a new Member',
-            'member': member
+        } else if (exist.length === 0) {
+          member.save(function(err, member) {
+            if (err) {
+              return res.status(500).json({
+                'error': 'Error saving new Member',
+                err: err || err.errmessage
+              });
+            } else if (member) {
+              return res.status(200).json({
+                'message': 'Successfully created a new Member',
+                'member': member
+              });
+            }
           });
         }
       });
