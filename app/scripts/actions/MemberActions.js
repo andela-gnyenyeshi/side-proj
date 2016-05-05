7d28154d649eb01217f472dd2c4710279ec09264
Gertrude <gertrude@Gertrudes-MacBook-Pro.local>
2016-05-05 17:00:48 +0300

    [#119008687] Modifies .babelrc

--- app/scripts/actions/MemberActions.js
+++ app/scripts/actions/MemberActions.js
@@ -1,17 +1,14 @@
-(function() {
+(() => {
   'use strict';
-  var Constants = require('../constants/MemberConstants');
+  let Constants = require('../constants/MemberConstants'),
   BaseActions = require('./BaseActions');
 
-  var MemberActions = {
-    registerMember: function(data) {
+  module.exports = {
+    registerMmember: function(data) {
       BaseActions.post('api/member', data, Constants.REG_MEMBER);
-    },
+    }
     confirmMember: function(data) {
-      console.log('Confirmed');
       BaseActions.get('api/member/' + data, Constants.CONFIRM_MEMBER);
     }
   };
-
-  module.exports = MemberActions;
 })();
